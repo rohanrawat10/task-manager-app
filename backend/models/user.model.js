17c95921578759bef952634ed6fc4c57817a504d
rohanrawat10 <rohanrawat10@users.noreply.github.com>
2026-02-13 14:49:54 +0530

    created user model

---
+++ backend/models/user.model.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema({
+    fullName:{
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    password:{
+             type:String,
+             required:true,
+    },
+    mobile:{
+        type:String,
+        required:true,
+    },
+    profileImageUrl:{
+        type:String,
+        default:"https://static.vecteezy.com/system/resources/previews/005/544/718/non_2x/profile-icon-design-free-vector.jpg"
+    
+    },
+    role:{
+        type:String,
+        enum:["admin","user"],
+        default:"user",
+    }
+},{
+    timestamps:true
+})
+
+const User = mongoose.model("User",userSchema);
+export default User;
