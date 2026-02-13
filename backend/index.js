ec903d4e4a904199b60dbeee013e253d526b8809
rohanrawat10 <rohanrawat10@users.noreply.github.com>
2026-02-13 13:17:14 +0530

    middleware to handle CORS

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,18 @@
 import express from "express";
+import cors from "cors";
 import http from "http"
 import dotenv from "dotenv";
 dotenv.config();
 const app = express()
 const port  = process.env.PORT || 5000;
 const server = http.createServer(app)
+//Middleware to handle cors
 
+app.use(cors({
+    origin:process.env.FRONTEND_URL || "http://localhost:5173",
+    methods:["GET","POST","PUT","DELETE"],
+    allowedHeaders:["Content-Type","Authorization"]
+}))
 
 
 server.listen(port,()=>{
