64eaf69892a194c558f9e5b7ffb2c1cd8717489e
rohanrawat10 <rohanrawat10@users.noreply.github.com>
2026-02-19 15:24:28 +0530

    update user profile api route

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,5 +1,7 @@
+import bcrypt from "bcrypt"
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js"
+
 
 export const userProfile = async(req,res,next)=>{
     try{
@@ -15,5 +17,58 @@ export const userProfile = async(req,res,next)=>{
     }
     catch(err){
         next(errorHandler(500,err.message))
+    }
+}
+
+// export const upadtedUserProfile = async(req,res,next)=>{
+//     try{
+//         const userId = req.userId
+//        const user = await User.findById(userId)
+//        if(!user){
+//         return next(errorHandler(400,"user not found"))
+//        }
+//        user.name = req.body.name || user.name
+//        user.email = req.body.email || user.email
+//         user.password = await bcrypt.hash(req.body.password,10)
+       
+//     //    const updatedData = {name,email,mobile,password,profileImageUrl,adminJoinCode}
+//     const updatedData = user   
+//     user = await User.findByIdAndUpdate(userId,updatedData,{new:true})
+//       return res.status(201).json(user)
+//     }
+//     catch(err){
+//         next(errorHandler(500,err.message))
+//     }
+// }
+
+
+export const upadteUserProfile = async(req,res,next)=>{
+    try{
+     const userId = req.userId;
+     const user = await User.findById(userId);
+     if(!user){
+        return next(errorHandler(400,"User not found"))
+     }
+     const {name,email,mobile,password,profileImageUrl,adminJoinCode} = req.body
+     let updatedFields = {
+        name,
+        email,
+        mobile,
+        profileImageUrl,
+        adminJoinCode
+     }
+     //only hash if password is provided
+     if(password){
+        updatedFields.password = await bcrypt.hash(password,10)
+     }
+     const updatedUser = await User.findByIdAndUpdate(
+        userId,
+        updatedFields,
+     {new:true}
+    )
+     return res.status(200).json(updatedUser)
+    }
+    catch(err){
+        next(errorHandler(400,err.message))
     }
 }
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,7 +1,7 @@
 import express from "express";
 import isAuth from "../middelwares/isAuth.js";
-import { userProfile } from "../controllers/user.controller.js";
+import { upadteUserProfile, userProfile } from "../controllers/user.controller.js";
 const userRouter = express.Router();
 userRouter.get("/user-profile",isAuth,userProfile)
-
+userRouter.put("/updated-profile",isAuth,upadteUserProfile)
 export default userRouter;
