2e7af843dabe4d504d5163c5caf0ad656d999f2c
rohanrawat10 <rohanrawat10@users.noreply.github.com>
2026-02-20 14:26:02 +0530

    get user api route

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { signUp, signIn } from "../controllers/auth.controller.js";
+import { signUp, signIn,userProfile,updatedUserProfile,uploadImage } from "../controllers/auth.controller.js";
 import isAuth from "../middelwares/isAuth.js";
 import { upload } from "../middelwares/multer.js";
 const authRouter = express.Router();
 authRouter.post("/sign-up",upload.single("image"),signUp)
 authRouter.post("/sign-in",signIn)
+authRouter.get("/user-profile",isAuth,upload.single('image'),userProfile)
+authRouter.put("/updated-profile",isAuth,upload.single("image"),upadteUserProfile)
+authRouter.post("/upload-image",upload.single("image"),uploadImage)
 // authRouter.get("/user-profile",isAuth,userProfile)
 export default authRouter;
