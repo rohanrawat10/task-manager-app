fd8ffa76b19903f8e3312f640d44dd769a872ad9
rohanrawat10 <rohanrawat10@users.noreply.github.com>
2026-04-02 19:34:31 +0530

    get users api

--- frontend/src/pages/admin/ManageUsers.jsx
+++ frontend/src/pages/admin/ManageUsers.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import DashboardLayout from '../../components/DashboardLayout'
+import axios from 'axios';
+import { serverUrl } from '../../config';
 
 function ManageUsers() {
+  const [allUsers,setAllUsers] = useState([]);
+  const getAllUsers = async ()=>{
+    try{
+     const result = await axios.get(`${serverUrl}/api/users/get-users`,{withCredentials:true})
+      if(result.data?.length>0){
+        setAllUsers(result.data)
+      }
+      console.log(result.data)
+    }
+    catch(err){
+        console.log("Get users error:",err.message)
+    }
+  }
+  useEffect(()=>{
+    getAllUsers()
+    return ()=>{}
+  },[])
   return (
-    <DashboardLayout>ManageUsers</DashboardLayout>
+    <DashboardLayout activeMenu={"Team Members"}>ManageUsers</DashboardLayout>
   )
 }
 
